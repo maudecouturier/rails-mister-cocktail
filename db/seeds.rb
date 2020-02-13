77917495e3f8247772f624a23529330610d3a297
Maude Couturier <maude.couturier2@gmail.com>
2020-02-13 16:38:43 +0100

    Mister cocktail avant CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+
+puts 'Destroying ingredients...'
+Ingredient.destroy_all
+
+puts 'Seeding ...'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+ingredient_array = user['drinks']
+
+ingredient_array.each do |i|
+  ingredient = Ingredient.new(name: i['strIngredient1'])
+  ingredient.save!
+end
+
+puts 'Finished!'
+
+puts 'Seeding cocktails'
+Cocktail.create(name: 'Mojito')
+Cocktail.create(name: 'Caipi')
+Cocktail.create(name: 'Aperol Spritz')
+Cocktail.create(name: 'Pisco Sour')
+Cocktail.create(name: 'Daiquiri')
+Cocktail.create(name: 'Bloody Mary')
+Cocktail.create(name: 'Cosmopolitan')
+Cocktail.create(name: 'Margarita')
+Cocktail.create(name: 'Moscow Mule')
+Cocktail.create(name: 'Martini')
+Cocktail.create(name: 'Sangria')
+puts "finished preparing cocktails"
+
+
